a571367a70bb04d78ef4c03cc898c9f252b2a37a
Richard Mac <78776822+richardmac604@users.noreply.github.com>
2024-10-07 21:20:40 -0700

    modes - dropdown - router implementation
    added routing to the app
    modes component
    dropdown menu component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,27 @@
 
 import './App.css'
+import Modes from './Components/Modes/Modes.tsx'
 import Navbar from './Components/Navbar/Navbar.tsx'
+import {Route, Routes} from "react-router-dom"
+import Profile from './pages/Profile.tsx'
+import Statistics from './pages/Statistics.tsx'
+import Settings from './pages/Settings.tsx'
 
 function App() {
-  
 
   return (
     <>
-   <Navbar></Navbar>
+   <Navbar/>
+   <div className='container'>
+    <Routes>
+      <Route path = "/statistics" element = {<Statistics/>}></Route>
+      <Route path = "/settings" element = {<Settings/>}></Route>
+      <Route path = "/profile" element = {<Profile/>}></Route>
+    </Routes>
+   </div>
+  
+   
+   <Modes/>
     </>
   )
 }
