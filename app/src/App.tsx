17621d1dbf4e4b7d05ae81a8fa906491396d6492
Shuang Wu <shuang111994@gmail.com>
2024-11-25 20:04:38 -0800

    fixed pages import

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,8 +7,8 @@ import Settings from './pages/Settings.tsx';
 import MusicType from './pages/MusicType.tsx';
 import NormalType from './pages/NormalType.tsx';
 import Modes from './Components/Modes/Modes.tsx';
-import Studying from './Pages/Studying.tsx'
-import StudyingActivity from './Pages/StudyingActivity.tsx'
+import Studying from './pages/Studying.tsx'
+import StudyingActivity from './pages/StudyingActivity.tsx'
 
 function App() {
  
